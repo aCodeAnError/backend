4b4c347cb21d9e9d09abe44a01aac22c2a47b204
aCodeAnError <rk8791137@gmail.com>
2024-10-21 16:48:39 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constansts.js"
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`)
+        console.log(connectionInstance)
+        console.log("separation")
+        console.log(connectionInstance.connection)
+        console.log("separation")
+        console.log(connectionInstance.connection.host)
+
+    }catch(error){
+        console.log("MONGODB connection error", error)
+        process.exit(1)// nodejs gives access to process that current program is running on
+    }
+}
+
+export default connectDB
